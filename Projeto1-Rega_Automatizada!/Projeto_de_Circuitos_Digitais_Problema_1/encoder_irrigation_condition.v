c8edb86160ef8da86643813ecb4493684d496360
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-04-23 02:31:46 -0300

    Changed bit0 and bit1 equations
    Now supports the irrigation decoder

--- Projeto1-Rega_Automatizada!/Projeto_de_Circuitos_Digitais_Problema_1/encoder_irrigation_condition.v
+++ Projeto1-Rega_Automatizada!/Projeto_de_Circuitos_Digitais_Problema_1/encoder_irrigation_condition.v
@@ -13,8 +13,8 @@ module new_encoder_irrigation_condition
 	wire w0, w1, w2, w3;
 	
 	// Reversing both outputs of irrigation systems
-	not not0(w0, sprinkler_status_i);
-	not not1(w1, drip_status_i);
+	not not0(w0, drip_status_i);
+	not not1(w1, sprinkler_status_i);
 	
 	// Applying AND gates between each irrigation
 	// system inverted and irrigation status
@@ -23,7 +23,7 @@ module new_encoder_irrigation_condition
 	
 	// Using XNOR gate into the AND gates outputs
 	// and the status of each irrigation system
-	xnor bit0_value(bit0_o, w2, drip_status_i); 
-	xnor bit1_value(bit1_o, w3, sprinkler_status_i);
+	xnor bit0_value(bit0_o, w2, sprinkler_status_i); 
+	xnor bit1_value(bit1_o, w3, drip_status_i);
 
 endmodule
