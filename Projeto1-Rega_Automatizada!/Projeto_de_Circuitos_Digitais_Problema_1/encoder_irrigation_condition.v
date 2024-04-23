9c9de2f32a4da703b94ec566714f24e435a6c431
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-04-23 20:15:21 -0300

    Atualized irrigation encoder
    Encoder to irrigation condition completed.

--- Projeto1-Rega_Automatizada!/Projeto_de_Circuitos_Digitais_Problema_1/encoder_irrigation_condition.v
+++ Projeto1-Rega_Automatizada!/Projeto_de_Circuitos_Digitais_Problema_1/encoder_irrigation_condition.v
@@ -1,6 +1,6 @@
 /* Encodes the irrigations states into three possibilities. */
 
-module new_encoder_irrigation_condition
+module encoder_irrigation_condition
 (
 	input  sprinkler_status_i,
 	input  drip_status_i,
@@ -16,14 +16,15 @@ module new_encoder_irrigation_condition
 	not not0(w0, drip_status_i);
 	not not1(w1, sprinkler_status_i);
 	
-	// Applying AND gates between each irrigation
-	// system inverted and irrigation status
-	and and0(w2, w0, irrigation_status_i);
-	and and1(w3, w1, irrigation_status_i);
+	// Applying XNOR gates between each 
+	// irrigation system and irrigation status
+	xnor xnor0(w2, irrigation_status_i, sprinkler_status_i);
+	xnor xnor1(w3, irrigation_status_i, drip_status_i);
 	
-	// Using XNOR gate into the AND gates outputs
-	// and the status of each irrigation system
-	xnor bit0_value(bit0_o, w2, sprinkler_status_i); 
-	xnor bit1_value(bit1_o, w3, drip_status_i);
+	// Using AND gate into the XNOR gates outputs
+	// and the status of each inverted irrigation system
+	and bit0_value(bit0_o, w0, w2);
+	and bit1_value(bit1_o, w1, w3);
+	
 
 endmodule
