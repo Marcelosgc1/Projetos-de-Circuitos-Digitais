8929cc66d61a06559e2e269f579dc16286641f2d
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-05-08 20:45:31 -0300

    Changed seconds timer and project configurations

--- Projeto2-Rega_Automatizada(melhorias)!/_60_seconds_timer.v
+++ Projeto2-Rega_Automatizada(melhorias)!/_60_seconds_timer.v
@@ -1,24 +1,28 @@
+/* Count from 59 to 0 seconds using the clock value */
+
 module _60_seconds_timer
 (
 	input wire clk,
 	input wire reset,
 	output reg [5:0] seconds_counter
 );
-
+	
+	// Declaring a register to compute seconds using the clock value
 	reg [25:0] clk_counter;
 	
-	always @(posedge clk or posedge rst) begin
-		if (rst) begin
+	// Behavioral logic to measure time and reset timer
+	always @(posedge clk or posedge reset) begin
+		if (reset) begin
 			clk_counter <= 26'd0;
 			seconds_counter <= 6'd59;
 		end else if (clk_counter == 26'd50_000_000) begin
 			clk_counter <= 26'd0;
-		end if (seconds_counter == 6'd0)
-			seg <= 6'd59;
-		end else begin
-			seconds_counter <= seconds_counter - 6'd1;
+			if (seconds_counter == 6'd0)
+				seconds_counter <= 6'd59;
+			else
+				seconds_counter <= seconds_counter - 6'd1;
+		end else
 			clk_counter <= clk_counter + 26'd1;
-		end
 	end
 
 endmodule
