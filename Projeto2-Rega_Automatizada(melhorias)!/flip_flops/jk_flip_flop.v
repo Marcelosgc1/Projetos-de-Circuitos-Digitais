608163a601f6fc36d04064528193631d69144f31
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-06-02 19:14:20 -0300

    Removing redundant JK flip-flop

--- Projeto2-Rega_Automatizada(melhorias)!/flip_flops/jk_flip_flop.v
+++ Projeto2-Rega_Automatizada(melhorias)!/flip_flops/jk_flip_flop.v
@@ -1,16 +1,29 @@
-/* */
+/* Implementation of a JK flip-flop with clear and preset inputs */
 
 module jk_flip_flop
 (
-	input j, k, clk, 
-	output reg q
+	input wire  clk, j, k, clear, preset
+	output reg  q, 
+	output wire q_bar
 );
-	always @ (posedge clk)
-		case ({j, k})
-			2'b00: q <= q;
-			2'b01: q <= 0;
-			2'b10: q <= 1;
-			2'b11: q <= ~q;
-		endcase
-		
+
+	always @ (posedge clk or posedge clear or posedge preset) begin
+		if (clear) begin
+			q <= 0;
+		end
+		else if (preset) begin
+			q <= 1;
+		end
+		else begin
+			case ({j,k})
+				2'b00: q <= q;
+				2'b01: q <= 0;
+				2'b10: q <= 1;
+				2'b11: q <= ~q;
+			endcase
+		end
+	end
+	
+	assign q_bar = ~q;
+
 endmodule
