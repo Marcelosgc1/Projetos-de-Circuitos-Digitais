6a30038d084b3d8f1d4b9be4a731d976653c96ad
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-06-05 22:43:37 -0300

    Adjusting the module to with the output of the presets

--- Projeto2-Rega_Automatizada(melhorias)!/high_level_checker.v
+++ Projeto2-Rega_Automatizada(melhorias)!/high_level_checker.v
@@ -1,49 +1,48 @@
-/* Sets the water tank level dinamically */
+/* Manipulates the higher level sensor dinamically */
 
 module high_level_checker
 (
-	input  wire [1:0] dozen_minutes_timer,
-	input  wire [2:0]	unit_minutes_timer,
-	input  wire 		high_level_indicator,
-	input  wire  		air_humidity,
-	input  wire  		soil_humidity,
-	input  wire  		temperature,
+	input  wire [1:0] dozen_minutes_timer, dozen_minutes_preset, dozen_seconds_timer
+	input  wire [2:0] unit_minutes_timer, 
+	input  wire			high_level_indicator,
 	output wire 		high_level_sensor
 );
-	wire [7:0] aux;
-	wire [1:0] inv_dozen_timer;
-	wire [1:0] inv_unit_timer;
-	wire inv_air_humidity, inv_soil_humidity, inv_temperature;
-	
+	wire [11:0] aux;
+	wire [ 1:0] inv_dozen_timer, inv_dozen_preset, inv_unit_timer, inv_unit_preset;
+
 	not inv0(inv_dozen_timer[0], dozen_minutes_timer[0]);
 	not inv1(inv_dozen_timer[1], dozen_minutes_timer[1]);
 	
-	not inv2(inv_unit_timer[0], unit_minutes_timer[0]);
-	not inv3(inv_unit_timer[1], unit_minutes_timer[1]);
+	not inv2(inv_dozen_preset[0], dozen_minutes_preset[0]);
+	not inv3(inv_dozen_preset[1], dozen_minutes_preset[1]);
 	
-	not inv4(inv_air_humidity, air_humidity);
-	not inv5(inv_soil_humidity, soil_humidity);
-	not inv6(inv_temperature, temperature);
+	not inv4(inv_unit_timer[0], unit_minutes_timer[0]);
+	not inv5(inv_unit_timer[1], unit_minutes_timer[1]);
+	
+	not inv6(inv_unit_preset[0], unit_minutes_preset[0]);
+	not inv7(inv_unit_preset[1], unit_minutes_preset[1]);
 	
 	// When the dozen of minutes is equal 3
 	and and0(aux[0], dozen_minutes_timer[1], dozen_minutes_timer[0]);
 	
-	// When the minutes counter is equal 15, 
-	// air humidity is low and the soil is dry
+	// When the minutes counter is equal 15 and the dozen minutes preset is 0001
 	and and1(aux[1], inv_dozen_timer[1], dozen_minutes_timer[0]);
-	and and2(aux[2], unit_minutes_timer[2], inv_unit_timer[1], unit_minutes_timer[0]);
+	and and3(aux[2], inv_dozen_preset[1], dozen_minutes_preset[0]);
+	and and2(aux[3], unit_minutes_timer[2], inv_unit_timer[1], unit_minutes_timer[0]);
 	
-	and and3(aux[3], aux[1], aux[2], inv_air_humidity, inv_soil_humidity);
+	and and5(aux[4], aux[1], aux[2], aux[3]);
 	
-	// When the minutes counter is equal 22, both air
-   //	humidity and temperature are high and the soil is dry 
-	and and4(aux[4], dozen_minutes_timer[1], inv_dozen_timer[0]);
-	and and5(aux[5], unit_minutes_timer[1], inv_unit_timer[0]);
+	// When the minutes counter is equal 22, the dozen of 
+	// seconds is 3 and the dozen minutes preset is 0010
+	and and6(aux[5], dozen_minutes_timer[1], inv_dozen_timer[0]);
+	and and7(aux[6], dozen_minutes_preset[1], inv_dozen_preset[0]);
+	and and8(aux[7], unit_minutes_timer[1], inv_unit_timer[0]);
+	and and9(aux[8], dozen_seconds_timer[0], dozen_seconds_timer[1]);
 	
-	and and6(aux[6], aux[4], aux[5], air_humidity, inv_temperature, inv_soil_humidity);
+	and and10(aux[9], aux[5], aux[6], aux[7], aux[8]);
 	
 	// Final expression
-	or  or0(aux[7], aux[0], aux[3], aux[6]);
-	and high_level_signal(high_level_sensor, high_level_indicator, aux[7]);
+	or  or0(aux[11], aux[0], aux[4], aux[9]);
+	and high_level_signal(high_level_sensor, high_level_indicator, aux[11]);
 
 endmodule
