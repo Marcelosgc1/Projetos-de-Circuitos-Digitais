8e9ce599b8c0a3c6149ad681a47ff8f12fd75757
Marcelo <89476116+Marcelosgc1@users.noreply.github.com>
2024-06-08 11:27:50 -0300

    Update level_state.v

--- Projeto2-Rega_Automatizada(melhorias)!/level_state.v
+++ Projeto2-Rega_Automatizada(melhorias)!/level_state.v
@@ -1,4 +1,4 @@
-module level_state(input clk, input H, input M, input L, input init, output [2:0]water_level_state);
+module level_state(input clk, input H, input M, input L, input init, output [3:0]water_level_state);
 
 	wire inv_low_level, inv_medium_level, inv_high_level;
 	
@@ -6,47 +6,62 @@ module level_state(input clk, input H, input M, input L, input init, output [2:0
 	not inv1(inv_medium_level, M);
 	not inv2(inv_high_level, H);
 	
+	
+	
 	//state 1
-	and(aux0, L, inv_medium_level,inv_high_level);
+	and(aux0, inv_low_level, inv_medium_level,inv_high_level);
 	not(aux0_bar, aux0);
 	
-	and(low_level, aux0, init);
-	and(low_level_inverse, aux0_bar, init);
+	and(crit_level, aux0, init);
+	and(crit_level_inverse, aux0_bar, init);
 	
 	
-	jk_flip_flop(0, 0, clk, low_level_inverse, low_level, water_level_state[0]);
-
+	jk_flip_flop(0, 0, clk, crit_level_inverse, crit_level, water_level_state[0]);
+	
+	
 	
 	//state 2
-	and(aux1, L, M, inv_high_level);
+	and(aux1, L, inv_medium_level,inv_high_level);
 	not(aux1_bar, aux1);
 	
-	and(middle_level, aux1, init);
-	and(middle_level_inverse, aux1_bar, init);
+	and(low_level, aux1, init);
+	and(low_level_inverse, aux1_bar, init);
 	
-	jk_flip_flop(0, 0, clk, middle_level_inverse, middle_level, water_level_state[1]);
+	
+	jk_flip_flop(0, 0, clk, low_level_inverse, low_level, water_level_state[1]);
+
+	
+	//state 3
+	and(aux2, L, M, inv_high_level);
+	not(aux2_bar, aux1);
+	
+	and(middle_level, aux2, init);
+	and(middle_level_inverse, aux2_bar, init);
+	
+	jk_flip_flop(0, 0, clk, middle_level_inverse, middle_level, water_level_state[2]);
 
 	
 	
 	
-	//state 3
-	and(aux2, L, M, H);
-	not(aux2_bar, aux2);
+	//state 4
+	and(aux3, L, M, H);
+	not(aux3_bar, aux2);
 	
-	and(high_level, aux2, init);
-	and(high_level_inverse, aux2_bar, init);
+	and(high_level, aux3, init);
+	and(high_level_inverse, aux3_bar, init);
 	
 	
-	jk_flip_flop(0, 0, clk, high_level_inverse, high_level, water_level_state[2]);
+	jk_flip_flop(0, 0, clk, high_level_inverse, high_level, water_level_state[3]);
 
 
 
 	/*
 	COMO FUNCIONA?
- 	001 -> nível baixo
-  	010 -> nível médio
-   	100 -> nível alto
-    	000 -> erro OU nivel crítico
+	0001 -> nível crítico
+ 	0010 -> nível baixo
+  	0100 -> nível médio
+   1000 -> nível alto
+   0000 -> erro
  	*/
 
 
